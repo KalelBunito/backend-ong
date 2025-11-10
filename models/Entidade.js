f7543b3a4b372d89bc6ff8ab1d75c976d2cb7c10
KalelBunito <kalelhbarboza@gmail.com>
2025-11-10 18:04:27 -0300

    featcriaçao do Model Entidade para o cadastro de Ongs

---
+++ models/Entidade.js
@@ -0,0 +1,23 @@
+class Entidade {
+    //  parâmetro
+    // toda classe possui propriedades e métodos
+    constructor(id, nome, cnpj, site) {
+        if (isNaN(id) || isNaN(cnpj)) {
+            throw new Error("A propriedade precisa ser um número válido")
+        }
+
+        if (String(cnpj).length < 14){
+            throw new Error("O CNPJ precisa ter pelo menos 14 dígitos")
+        }
+        this.id = id
+        this.nome = nome
+        this.cnpj = cnpj
+        this.site = site
+        this.mensagem = "ONG cadastrada com sucesso"
+    }
+}
+
+const ong1 = new Entidade("2", "silvestre em perigo", "23232", "silvestresemperigo.com.br")
+const ong2 = new Entidade(3, "tutu o tatu", 16478275837571845, "tutuotatu.com.br")
+console.log(ong1);
+console.log(ong2);
